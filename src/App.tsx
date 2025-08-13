27610a0d6868c6d06e72c53b953f752b3ecf0a26
Nick-Dev-Ops <nickgofman1@gmail.com>
2025-08-13 16:28:34 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import Quiz from "./pages/Quiz";
+import Practice from "./pages/Practice";
+
 import NotFound from "./pages/NotFound";
 import { HelmetProvider } from "react-helmet-async";
 
@@ -20,6 +22,8 @@ const App = () => (
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/Quiz" element={<Quiz />} />
+            <Route path="/Practice" element={<Practice />} />
+
             {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
             <Route path="*" element={<NotFound />} />
           </Routes>

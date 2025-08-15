fa0266b3798445ba388eb0725bef1a74d582ebde
NickGofman <83462964+NickGofman@users.noreply.github.com>
2025-08-15 22:13:37 +0300

    add ScrollToTop

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar"; // adjust path if needed
 import { Outlet } from "react-router-dom";
+import ScrollToTop from "./ScrollToTop";
 
 const Layout = () => {
   return (
     <>
+      <ScrollToTop />
       <Navbar />
       <main className="min-h-screen bg-gradient-to-br from-background via-background to-muted/20 px-4 py-6 md:py-8">
         <Outlet />
